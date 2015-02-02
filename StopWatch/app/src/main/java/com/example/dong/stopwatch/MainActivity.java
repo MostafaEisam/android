5b72070d90a451897e9c891fdf5ce9cd5e0a5ba1
Dong Hee Kim <opklnm102@naver.com>
2015-02-03 01:34:48 +0900

    change1

--- StopWatch/app/src/main/java/com/example/dong/stopwatch/MainActivity.java
+++ StopWatch/app/src/main/java/com/example/dong/stopwatch/MainActivity.java
@@ -4,60 +4,40 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     TextView mV, sV, oHsV;
     Button sBtn, pBtn, rBtn;
-    int time = 0, sTime = 0, mTime = 0;
+    //int time = 0, sTime = 0, mTime = 0;
     String zero = "0";
-
-    Thread mThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            while(Thread.currentThread() == mThread){
-                Message msg = new Message();
-                time++;
-                if (time == 10) {
-                    time = 0;
-                    sTime++;
-                }
-                if (sTime == 60) {
-                    sTime = 0;
-                    mTime++;
-                }
-                //보낸다.
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                }
-            }
-        }
-    });
+    boolean threadStart = false;
 
     final Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            if (msg.what == 1) {
-                if (0 <= mTime && mTime <= 9)
-                    mV.setText(zero + String.valueOf(mTime));
+            if (msg.what == 3) {  //분 set
+                oHsV.setText("0");
+                sV.setText("00");
+                if (0 <= msg.arg1 && msg.arg1 <= 9)
+                    mV.setText(zero + String.valueOf(msg.arg1));
                 else
-                    mV.setText(String.valueOf(mTime));
-            } else if (msg.what == 2) {
-                if (0 <= mTime && mTime <= 9)
-                    mV.setText(zero + String.valueOf(mTime));
+                    mV.setText(String.valueOf(msg.arg1));
+            } else if (msg.what == 2) {  //초 set
+                oHsV.setText("0");
+                if (0 <= msg.arg1 && msg.arg1 <= 9)
+                    sV.setText(zero + String.valueOf(msg.arg1));
                 else
-                    mV.setText(String.valueOf(mTime));
-            } else if (msg.what == 3) {
-                oHsV.setText(String.valueOf(time));
+                    sV.setText(String.valueOf(msg.arg1));
+            } else if (msg.what == 1) {  //0.1초 set
+                oHsV.setText(String.valueOf(msg.arg1));
             }
         }
     };
 
+    MyThread mThread = new MyThread(handler);  //쓰레드 생성
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,5 +51,29 @@ public class MainActivity extends ActionBarActivity {
         sBtn = (Button) findViewById(R.id.Start);
         pBtn = (Button) findViewById(R.id.Pause);
         rBtn = (Button) findViewById(R.id.Reset);
+        sBtn.setOnClickListener(this);
+        pBtn.setOnClickListener(this);
+        rBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+         switch (v.getId()) {
+            case R.id.Start:  //시작
+               if(threadStart)
+                    mThread.watchStart();
+                else {
+                    mThread.watchStart();
+                   mThread.start();  //쓰레드 시작
+                   threadStart = true;
+                }
+                break;
+            case R.id.Pause:  //일시정지
+                mThread.watchStop();
+                break;
+            case R.id.Reset:  //재시작
+                mThread.watchRestart();
+                break;
+        }
     }
 }
--- StopWatch/app/src/main/java/com/example/dong/stopwatch/MyThread.java
+++ StopWatch/app/src/main/java/com/example/dong/stopwatch/MyThread.java
@@ -1,8 +1,71 @@
 package com.example.dong.stopwatch;
 
+import android.os.Handler;
+import android.os.Message;
+
 /**
  * Created by 102-9 on 2015-01-16.
  */
-public class MyThread {
+public class MyThread extends Thread {
+    private int time = 0, sTime = 0, mTime = 0;  //0.1초, 초, 분
+    private boolean isRunnable = false;
+    Handler handler;
 
+    MyThread(Handler handler) {
+        this.handler = handler;
+    }
+
+    public void watchStart(){  //시작 메소드
+        isRunnable = true;
+    }
+
+    public void watchStop(){  //일시정지 메소드
+        isRunnable = false;
+    }
+
+    public void watchRestart(){  //재시작하기위해 쓰레드 종료
+        time = 0;
+        sTime = 0;
+        mTime = 0;
+        Message msg = new Message();
+
+        msg.what = 3;
+        msg.arg1 = mTime;
+        handler.sendMessage(msg);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            while (isRunnable) {
+                        Message msg = new Message();
+
+                        try {
+                            Thread.sleep(100);  //0.1초의 딜레이
+                        } catch (InterruptedException e) {
+                        }
+
+                        msg.what = 1;
+                        time++;  //시간증가
+                        msg.arg1 = time;
+
+                        if (time == 10) {  //1초
+                            msg.what = 2;
+                            time = 0;
+                            msg.arg2 = time;
+                            sTime++;
+                            msg.arg1 = sTime;
+                        }
+                        if (sTime == 60) {  //1분
+                            msg.what = 3;
+                            sTime = 0;
+                            time = 0;
+                            msg.arg2 = sTime;
+                            mTime++;
+                    msg.arg1 = mTime;
+                }
+                handler.sendMessage(msg);
+            }
+        }
+    }
 }
