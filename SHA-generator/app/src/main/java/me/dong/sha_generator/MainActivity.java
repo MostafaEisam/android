64d15464512cb96f6d613eca1b9e66b7c32b8d7c
DongHeeKim <opklnm102@naver.com>
2016-09-09 23:24:52 +0900

    sha-generator 추가

---
+++ SHA-generator/app/src/main/java/me/dong/sha_generator/MainActivity.java
@@ -0,0 +1,39 @@
+package me.dong.sha_generator;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getAppKeyHash();
+    }
+
+    private void getAppKeyHash() {
+
+        try {
+            PackageInfo info = getPackageManager()
+                    .getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.d("Hash Key", something);
+            }
+        } catch (PackageManager.NameNotFoundException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+}
